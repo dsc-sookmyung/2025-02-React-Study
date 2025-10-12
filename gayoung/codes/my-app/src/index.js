50d54d4c5c92fb7e8a23c134e0ba9ae1ce7e4568
gana218 <gana218@naver.com>
2025-10-13 00:59:00 +0900

    Update index.js

--- gayoung/codes/my-app/src/index.js
+++ gayoung/codes/my-app/src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 //import Library from './chapter_03/Library';
 //import Clock from "./chapter_04/Clock";
 
-import CommentList from './chapter_05/CommentList';
+//import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
