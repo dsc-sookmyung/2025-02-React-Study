1ab36c96c291d429f33710175b16590703f832a0
gana218 <gana218@naver.com>
2025-10-13 00:51:07 +0900

    [STUDY] 3주차 유가영

--- gayoung/codes/my-app/src/index.js
+++ gayoung/codes/my-app/src/index.js
@@ -9,11 +9,12 @@ import reportWebVitals from './reportWebVitals';
 //import Clock from "./chapter_04/Clock";
 
 import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CommentList />
+    <NotificationList />
   </React.StrictMode>
 );
 
