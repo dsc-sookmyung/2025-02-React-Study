0948283824e3e4330b016477dc421fddd75cec28
dy0401kwon <dy0401kwon@naver.com>
2025-11-12 18:44:06 +0900

    [STUDY] 6주차 권도연

---
+++ doyeon/codes/myapp/src/chapter_16/component/ui/Button.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+    padding: 8px 16px;
+    font-size: 16px;
+    border-width: 1px;
+    border-radius: 8px;
+    cursor: pointer; 
+    background-color: lightgrey;
+    border: none;
+
+    &:hover {
+        background-color: grey;
+        color: white;
+    }
+    `;
+
+    function Button(props) {
+    const { title, onClick } = props; 
+    return <StyledButton onClick={onClick}>{title}</StyledButton>;
+    }
+
+export default Button;
